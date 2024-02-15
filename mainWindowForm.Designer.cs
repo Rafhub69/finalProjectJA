53e1e7ba4eec7eeed427ca042c6ebb2a2938881f
Rafhub69 <56224419+Rafhub69@users.noreply.github.com>
2024-02-15 11:20:54 +0100

    Ostateczne zmiany 1

--- mainWindowForm.Designer.cs
+++ mainWindowForm.Designer.cs
@@ -1,12 +1,16 @@
-﻿namespace finalProjectJA
+﻿namespace FinalProgramJA
 {
-    partial class mainWindowForm
+    partial class Labirynt
     {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private string[] tableSize = { "5", "7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29", "31", "33", "35", "37"};
+
+        /// <summary>
+        ///  Maze sizes
+        /// </summary>
+        private string[] tableSize = { "5", "7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29", "31", "33", "35", "37" };
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -29,141 +33,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindowForm));
-            button1 = new Button();
-            comboBox1 = new ComboBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Labirynt));
             label1 = new Label();
             label2 = new Label();
-            comboBox2 = new ComboBox();
-            hScrollBar1 = new HScrollBar();
             label3 = new Label();
-            label4 = new Label();
+            heightBox = new ComboBox();
+            widthBox = new ComboBox();
+            button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             pictureBox1 = new PictureBox();
+            label4 = new Label();
             radioButton1 = new RadioButton();
             radioButton2 = new RadioButton();
             label5 = new Label();
+            hScrollBar1 = new HScrollBar();
             label6 = new Label();
-            timer1 = new System.Windows.Forms.Timer(components);
             label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             saveFileDialog1 = new SaveFileDialog();
-            button3 = new Button();
-            button4 = new Button();
-            fileSystemWatcher1 = new FileSystemWatcher();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(60, 53);
-            button1.Name = "button1";
-            button1.Size = new Size(96, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Stwórz labirynt";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // comboBox1
-            // 
-            comboBox1.AccessibleDescription = "wybór wysokości";
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(77, 1);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(58, 23);
-            comboBox1.TabIndex = 1;
-            comboBox1.Text = "5";
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(8, 4);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(63, 15);
-            label1.TabIndex = 2;
+            label1.TabIndex = 0;
             label1.Text = "Wysokość:";
-            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(8, 27);
+            label2.Location = new Point(12, 33);
             label2.Name = "label2";
-            label2.Size = new Size(65, 15);
-            label2.TabIndex = 3;
-            label2.Text = "Szerokość: ";
-            label2.Click += label2_Click;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(77, 24);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(58, 23);
-            comboBox2.TabIndex = 4;
-            comboBox2.Text = "5";
-            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
-            // 
-            // hScrollBar1
-            // 
-            hScrollBar1.LargeChange = 1;
-            hScrollBar1.Location = new Point(96, 954);
-            hScrollBar1.Maximum = 64;
-            hScrollBar1.Minimum = 1;
-            hScrollBar1.Name = "hScrollBar1";
-            hScrollBar1.Size = new Size(1799, 17);
-            hScrollBar1.TabIndex = 5;
-            hScrollBar1.Value = 1;
-            hScrollBar1.Scroll += hScrollBar1_Scroll;
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Szerokość:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(8, 954);
+            label3.Location = new Point(144, 9);
             label3.Name = "label3";
-            label3.Size = new Size(69, 15);
-            label3.TabIndex = 6;
-            label3.Text = "Ilość rdzeni:";
-            label3.Click += label3_Click;
+            label3.Size = new Size(144, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Czas wykonania labiryntu:";
+            // 
+            // heightBox
+            // 
+            heightBox.FormattingEnabled = true;
+            heightBox.Location = new Point(80, 6);
+            heightBox.Name = "heightBox";
+            heightBox.Size = new Size(54, 23);
+            heightBox.TabIndex = 3;
+            heightBox.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // widthBox
+            // 
+            widthBox.FormattingEnabled = true;
+            widthBox.Location = new Point(80, 30);
+            widthBox.Name = "widthBox";
+            widthBox.Size = new Size(54, 23);
+            widthBox.TabIndex = 4;
+            widthBox.SelectedIndexChanged += widthBox_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Stwórz labirynt";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(155, 59);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Rozwiąż labirynt";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(25, 88);
+            button3.Name = "button3";
+            button3.Size = new Size(109, 23);
+            button3.TabIndex = 7;
+            button3.Text = "Zapisz labirynt";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(155, 88);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 23);
+            button4.TabIndex = 8;
+            button4.Text = "Zapisz rozwiązany labirynt";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 117);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1880, 762);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(80, 954);
+            label4.Location = new Point(12, 897);
             label4.Name = "label4";
-            label4.Size = new Size(13, 15);
-            label4.TabIndex = 7;
-            label4.Text = "1";
-            label4.Click += label4_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(47, 82);
-            button2.Name = "button2";
-            button2.Size = new Size(123, 23);
-            button2.TabIndex = 8;
-            button2.Text = "Rozwiąż labirynt";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(8, 111);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1884, 757);
-            pictureBox1.TabIndex = 9;
-            pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click;
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Biblioteka:";
             // 
             // radioButton1
             // 
             radioButton1.AutoSize = true;
             radioButton1.Checked = true;
-            radioButton1.Location = new Point(21, 904);
+            radioButton1.Location = new Point(13, 924);
             radioButton1.Name = "radioButton1";
             radioButton1.Size = new Size(49, 19);
-            radioButton1.TabIndex = 12;
+            radioButton1.TabIndex = 11;
             radioButton1.TabStop = true;
             radioButton1.Text = "C++";
             radioButton1.UseVisualStyleBackColor = true;
@@ -172,123 +175,128 @@
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(21, 925);
+            radioButton2.Location = new Point(12, 949);
             radioButton2.Name = "radioButton2";
             radioButton2.Size = new Size(75, 19);
-            radioButton2.TabIndex = 13;
+            radioButton2.TabIndex = 12;
             radioButton2.Text = "Asembler";
             radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(8, 880);
+            label5.Location = new Point(12, 992);
             label5.Name = "label5";
-            label5.Size = new Size(62, 15);
-            label5.TabIndex = 14;
-            label5.Text = "Biblioteka:";
-            label5.Click += label5_Click;
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Ilość rdzeni:";
+            // 
+            // hScrollBar1
+            // 
+            hScrollBar1.LargeChange = 1;
+            hScrollBar1.Location = new Point(109, 990);
+            hScrollBar1.Maximum = 64;
+            hScrollBar1.Minimum = 1;
+            hScrollBar1.Name = "hScrollBar1";
+            hScrollBar1.Size = new Size(1783, 17);
+            hScrollBar1.TabIndex = 14;
+            hScrollBar1.Value = 1;
+            hScrollBar1.Scroll += hScrollBar1_Scroll;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(210, 4);
+            label6.Location = new Point(87, 990);
             label6.Name = "label6";
-            label6.Size = new Size(152, 15);
+            label6.Size = new Size(13, 15);
             label6.TabIndex = 15;
-            label6.Text = "Czas wykonania algorytmu:";
-            // 
-            // timer1
-            // 
-            timer1.Tick += timer1_Tick;
+            label6.Text = "1";
+            label6.Click += label6_Click;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(368, 4);
+            label7.Location = new Point(144, 33);
             label7.Name = "label7";
-            label7.Size = new Size(13, 15);
+            label7.Size = new Size(148, 15);
             label7.TabIndex = 16;
-            label7.Text = "0";
-            label7.Click += label7_Click;
+            label7.Text = "Czas rozwiązania labiryntu:";
             // 
-            // button3
+            // label8
             // 
-            button3.Location = new Point(214, 53);
-            button3.Name = "button3";
-            button3.Size = new Size(132, 23);
-            button3.TabIndex = 19;
-            button3.Text = "Zapisz labirynt";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
+            label8.AutoSize = true;
+            label8.Location = new Point(294, 9);
+            label8.Name = "label8";
+            label8.Size = new Size(13, 15);
+            label8.TabIndex = 17;
+            label8.Text = "1";
+            label8.Click += label8_Click;
             // 
-            // button4
+            // label9
             // 
-            button4.Location = new Point(194, 82);
-            button4.Name = "button4";
-            button4.Size = new Size(171, 23);
-            button4.TabIndex = 20;
-            button4.Text = "Zapisz rozwiązany labirynt";
-            button4.UseVisualStyleBackColor = true;
+            label9.AutoSize = true;
+            label9.Location = new Point(294, 33);
+            label9.Name = "label9";
+            label9.Size = new Size(13, 15);
+            label9.TabIndex = 18;
+            label9.Text = "1";
+            label9.Click += label9_Click;
             // 
-            // fileSystemWatcher1
-            // 
-            fileSystemWatcher1.EnableRaisingEvents = true;
-            fileSystemWatcher1.SynchronizingObject = this;
-            // 
-            // mainWindowForm
+            // Labirynt
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1904, 1011);
-            Controls.Add(button4);
-            Controls.Add(button3);
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(label9);
+            Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(label6);
+            Controls.Add(hScrollBar1);
             Controls.Add(label5);
             Controls.Add(radioButton2);
             Controls.Add(radioButton1);
-            Controls.Add(pictureBox1);
-            Controls.Add(button2);
             Controls.Add(label4);
+            Controls.Add(pictureBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(widthBox);
+            Controls.Add(heightBox);
             Controls.Add(label3);
-            Controls.Add(hScrollBar1);
-            Controls.Add(comboBox2);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(comboBox1);
-            Controls.Add(button1);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Name = "mainWindowForm";
+            Name = "Labirynt";
             Text = "Labirynt";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private ComboBox comboBox1;
         private Label label1;
         private Label label2;
-        private ComboBox comboBox2;
-        private HScrollBar hScrollBar1;
         private Label label3;
-        private Label label4;
+        private ComboBox heightBox;
+        private ComboBox widthBox;
+        private Button button1;
         private Button button2;
+        private Button button3;
+        private Button button4;
         private PictureBox pictureBox1;
+        private Label label4;
         private RadioButton radioButton1;
         private RadioButton radioButton2;
         private Label label5;
+        private HScrollBar hScrollBar1;
         private Label label6;
-        private System.Windows.Forms.Timer timer1;
         private Label label7;
+        private Label label8;
+        private Label label9;
         private SaveFileDialog saveFileDialog1;
-        private Button button3;
-        private Button button4;
-        private FileSystemWatcher fileSystemWatcher1;
     }
 }
--- mainWindowForm.cs
+++ mainWindowForm.cs
@@ -1,28 +1,34 @@
-namespace finalProjectJA
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+
+namespace FinalProgramJA
 {
-    using System.Drawing;
-    using System.Windows.Forms;
-    using System.Runtime.InteropServices;
 
-    public partial class mainWindowForm : Form
+
+    public partial class Labirynt : Form
     {
-        Bitmap image;
-        private int myImageId;
-        private int cellWidth;
-        private int cellHeight;
-        private bool[,] labirynth;
-        private bool loadedLibrary;
-        private int labirynthWidth;
-        private int labirynthHeight;
-        private int[] intTableSize = { 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37 };
 
-        [DllImport(@"C:\Users\Rafa許source\repos\finalProjectJA\x64\Debug\mazeCreatorC.dll")]
-        public static extern bool createLabirynt(int height, int width);
-
-        [DllImport(@"C:\Users\Rafa許source\repos\finalProjectJA\x64\Debug\mazeCreatorAsembler.dll")]
+        [DllImport(@"C:\Users\Rafa許source\repos\TestProgram\x64\Debug\add.dll")]
         static extern int MyProc1(int a, int b);
 
-        public mainWindowForm()
+        [DllImport(@"C:\Users\Rafa許source\repos\TestProgram\x64\Debug\addC.dll")]
+        static extern int createLabirynt(int coreNumber, int labiryntWidth, int labiryntHeight, int cellSize);
+
+        private int[] intTableSize = { 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37 };
+        private int labirynthHeight;
+        private bool loadedLibrary;
+        private int labirynthWidth;
+        private bool[,] labirynth;
+        private int cellHeight;
+        private int myImageId;
+        private int cellWidth;
+        Bitmap image;
+
+
+        public Labirynt()
         {
             InitializeComponent();
 
@@ -30,25 +36,27 @@ namespace finalProjectJA
             {
                 if (i < 6)
                 {
-                    comboBox1.Items.Add(tableSize.GetValue(i));
+                    heightBox.Items.Add(tableSize.GetValue(i));
                 }
 
-                comboBox2.Items.Add(tableSize.GetValue(i));
+                widthBox.Items.Add(tableSize.GetValue(i));
             }
 
-            int x = 5, y = 3;
-            myImageId = MyProc1(x, y); ;
-            cellWidth = 50;
+            heightBox.Text = tableSize[0];
+            widthBox.Text = tableSize[0];
             cellHeight = 50;
-            loadedLibrary = createLabirynt(2,10); ;
+            cellWidth = 50;
+
+
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             if (labirynthHeight == 0)
             {
@@ -73,14 +81,29 @@ namespace finalProjectJA
             int totalWidth = labirynthWidth * cellWidth;
             int totalHeight = labirynthHeight * cellHeight;
 
-            if(loadedLibrary)
-            {
-                //c++
+            Stopwatch stopwatch = new Stopwatch();
 
+            if (loadedLibrary)
+            {
+                stopwatch.Start();
+
+                //c++
+               int lab = createLabirynt(hScrollBar1.Value, labirynthWidth, labirynthHeight, cellWidth);
+
+                stopwatch.Stop();
+
+                label8.Text = stopwatch.ElapsedMilliseconds.ToString();
             }
             else
             {
+
+                stopwatch.Start();
+
                 //asembler
+
+                stopwatch.Stop();
+
+                label8.Text = stopwatch.ElapsedMilliseconds.ToString();
 
             }
 
@@ -100,7 +123,7 @@ namespace finalProjectJA
                     rectangle = new Rectangle(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
                     graphics.DrawRectangle(blackPen, rectangle);
 
-                    if(labirynth[i, j] == true)
+                    if (labirynth[i, j] == true)
                     {
                         //graphics.FillRectangle(whiteBrush, rectangle);
 
@@ -119,41 +142,96 @@ namespace finalProjectJA
                         graphics.DrawLine(blackPen, totalWidth, totalHeight, totalWidth, 0);
                     }
 
-                    
+
                 }
             }
-
-
 
             string filePath = "image" + myImageId + ".jpg";
             image.Save(filePath);
             myImageId++;
 
             pictureBox1.Image = System.Drawing.Image.FromFile(filePath);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filePath = "solvedMaze" + myImageId + ".jpg";
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = filePath;
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                fs.Close();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
 
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            labirynthHeight = intTableSize[comboBox1.SelectedIndex];
+            labirynthHeight = intTableSize[heightBox.SelectedIndex];
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
+            string filePath = "maze" + myImageId + ".jpg";
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = filePath;
+            saveFileDialog1.ShowDialog();
 
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                fs.Close();
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            Stopwatch stopwatch = new Stopwatch();
 
+            if (loadedLibrary)
+            {
+                stopwatch.Start();
+
+                //c++
+                int lab = createLabirynt(hScrollBar1.Value, labirynthWidth, labirynthHeight, cellWidth);
+
+                stopwatch.Stop();
+
+                label9.Text = stopwatch.ElapsedMilliseconds.ToString();
+            }
+            else
+            {
+
+                stopwatch.Start();
+
+                //asembler
+
+                stopwatch.Stop();
+
+                label9.Text = stopwatch.ElapsedMilliseconds.ToString();
+
+            }
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
-
+            label6.Text = hScrollBar1.Value.ToString();
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
         }
@@ -163,19 +241,9 @@ namespace finalProjectJA
 
         }
 
-        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        private void widthBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            label4.Text = e.NewValue.ToString();
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
+            labirynthWidth = intTableSize[widthBox.SelectedIndex];
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -183,31 +251,14 @@ namespace finalProjectJA
 
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            labirynthWidth = intTableSize[comboBox2.SelectedIndex];
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label8_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void label9_Click(object sender, EventArgs e)
         {
-            string filePath = "image" + myImageId + ".jpg";
-            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp";
-            saveFileDialog1.Title = "Save an Image File";
-            saveFileDialog1.FileName = filePath;
-            saveFileDialog1.ShowDialog();
 
-            if (saveFileDialog1.FileName != "")
-            {
-                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
-                
-                image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
-                fs.Close();
-            }
         }
     }
 }
